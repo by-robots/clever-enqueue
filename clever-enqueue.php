1ad3ead4f58e3be0b7b10eafb98fef7a13763e84
By Robots <skynet@by-robots.com>
2018-04-17 13:55:00 +0100

    Require PHP5.6+.

--- clever-enqueue.php
+++ clever-enqueue.php
@@ -4,6 +4,7 @@
  * Plugin URI: https://clever-enqueue.by-robots.com
  * Description: Allows for conditional enqueuing of Javascript and CSS assets in WordPress.
  * Version: 0.1
+ * Requires PHP: 5.6
  * Author: By Robots
  * Author URI: https://www.by-robots.com
  * License: MIT
