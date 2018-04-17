197d46bf92ff5c9bd10e0e0da037a88063343f6f
By Robots <skynet@by-robots.com>
2018-04-17 12:06:00 +0100

    Structure the plugin a bit.

--- clever-enqueue.php
+++ clever-enqueue.php
@@ -9,3 +9,28 @@
  * Author URI: https://www.by-robots.com
  * License: MIT
  */
+
+// No direct access
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Include the main WooCommerce class.
+if ( ! class_exists( 'Clever_Enqueue' ) ) {
+	include_once dirname( __FILE__ ) . '/includes/class-clever-enqueue.php';
+}
+
+/**
+ * Main instance of WooCommerce.
+ *
+ * Returns the main instance of WC to prevent the need to use globals.
+ *
+ * @since  2.1
+ * @return \Clever_Enqueue
+ */
+function clever_enqueue() {
+	return new Clever_Enqueue;
+}
+
+// Global for backwards compatibility.
+$GLOBALS['clever_enqueue'] = clever_enqueue();
---
+++ includes/class-clever-enqueue.php
@@ -0,0 +1,17 @@
+<?php
+
+class Clever_Enqueue {
+	/**
+	 * Set-up the plugin.
+	 */
+	public function __construct() {
+		register_activation_hook( WC_PLUGIN_FILE, array( $this, 'install' ) );
+	}
+
+	/**
+	 * Install the plugin.
+	 */
+	public function install() {
+		//
+	}
+}
