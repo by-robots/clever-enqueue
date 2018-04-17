7840423802d5d12455a8d4b151e62ebf43db2287
By Robots <skynet@by-robots.com>
2018-04-17 11:44:55 +0100

    Get tests running.

---
+++ clever-enqueue.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Clever Enqueue
+ * Plugin URI: https://clever-enqueue.by-robots.com
+ * Description: Allows for conditional enqueuing of Javascript and CSS assets in WordPress.
+ * Version: 0.1
+ * Author: By Robots
+ * Author URI: https://www.by-robots.com
+ * License: MIT
+ */
